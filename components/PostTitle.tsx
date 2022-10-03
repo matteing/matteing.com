9fb445506770ce23751ad5b3ba0ac2500bd1a051
Sergio Mattei <sergiomattei@outlook.com>
2022-10-03 04:43:46 -0400

    Remove garbage package

--- components/PostTitle.tsx
+++ components/PostTitle.tsx
@@ -1,21 +1,18 @@
 import format from "date-fns/format";
 import { BaseProps, Post } from "../types";
-import readingTime from "reading-time";
 
 // TODO
 export default function PostTitle({
 	post,
 	className,
 }: { post: Post } & BaseProps) {
-	const readEstimate = readingTime(post.body);
 	return (
 		<div>
 			<div
 				className={`mx-auto mt-12 sm:max-w-[655px] md:mb-6 lg:mt-20 lg:mb-10 lg:max-w-[812px] ${className}`}
 			>
 				<div className="heading mb-2 text-gray-500">
-					{format(new Date(post.createdAt), "MMM d, y")} ·{" "}
-					{Math.ceil(readEstimate.minutes)} min read
+					{format(new Date(post.createdAt), "MMM d, y")}
 				</div>
 				<h1 className="mb-3 text-3xl font-bold tracking-tight lg:text-5xl">
 					{post.title}
