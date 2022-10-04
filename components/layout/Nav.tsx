c8fc8e908d7a616921e9e84f2a1c1004bce561e5
Sergio Mattei <sergiomattei@outlook.com>
2022-10-04 00:27:29 -0400

    Make pill nav responsive.

--- components/layout/Nav.tsx
+++ components/layout/Nav.tsx
@@ -21,7 +21,7 @@ const ROUTES: RouteDefinition[] = [
 		name: "Home",
 		href: "/",
 		icon: <HomeIcon />,
-		className: "rounded-l-full",
+		className: "md:rounded-l-full",
 	},
 	{
 		name: "Work",
@@ -39,7 +39,7 @@ const ROUTES: RouteDefinition[] = [
 		href: "/more",
 		basePath: false,
 		icon: <BoltIcon />,
-		className: "rounded-r-full",
+		className: "md:rounded-r-full",
 	},
 ];
 
@@ -60,53 +60,19 @@ export function NavItem({
 		<Link href={href}>
 			<div
 				className={
-					"group flex cursor-pointer select-none items-center px-6 py-4 font-medium text-gray-700 transition-all hover:bg-purple-50 dark:text-gray-200 dark:hover:bg-purple-900 dark:hover:bg-opacity-30 " +
+					"group flex flex-1 shrink-0 cursor-pointer select-none items-center justify-center py-4 font-medium text-gray-700 transition-all hover:bg-purple-50 dark:text-gray-200 dark:hover:bg-purple-900 dark:hover:bg-opacity-30 md:justify-start md:px-6 " +
 					className +
 					(router.pathname === href
 						? " text-purple-600 dark:text-purple-500"
 						: "")
 				}
 			>
-				<div className="flex items-center transition-all group-active:scale-90">
+				<div className="flex flex-col items-center text-center transition-all group-active:scale-90 md:flex-row md:text-left">
 					{cloneElement((icon as ReactElement) ?? null, {
-						className: "mr-3 h-4 w-4",
+						className: "mb-1 md:mb-0 md:mr-3 md:h-4 md:w-4 h-6 w-6",
 					})}
 					<span className="leading-none">{children}</span>
 				</div>
-			</div>
-		</Link>
-	);
-}
-
-export function MobileNavItem({
-	icon,
-	children,
-	href,
-}: {
-	href: string;
-	icon?: JSX.Element;
-	basePath?: boolean;
-} & PropsWithChildren) {
-	const router = useRouter();
-
-	return (
-		<Link href={href}>
-			<div
-				className={
-					"group flex flex-1 shrink-0 cursor-pointer select-none flex-col items-center px-2 py-4 text-xs font-medium text-gray-700 transition-all hover:bg-purple-50 dark:text-gray-200 dark:hover:bg-purple-900 dark:hover:bg-opacity-30 " +
-					(router.pathname === href
-						? " text-purple-600 dark:text-purple-500"
-						: "")
-				}
-			>
-				{cloneElement((icon as ReactElement) ?? null, {
-					className: `mb-1 h-6 w-6 text-gray-500 ${
-						router.pathname === href
-							? " text-purple-600 dark:text-purple-500"
-							: ""
-					}`,
-				})}
-				{children}
 			</div>
 		</Link>
 	);
@@ -118,7 +84,7 @@ export function NavPills({
 	return (
 		<div
 			className={
-				"inter interactable flex rounded-full border bg-white shadow-sm transition-all dark:border-gray-700 dark:bg-gray-800 " +
+				"inter interactable flex w-full bg-white transition-colors dark:border-gray-700 dark:bg-gray-800 md:w-auto md:rounded-full md:border md:shadow-sm " +
 				className
 			}
 		>
@@ -131,26 +97,13 @@ export function NavPills({
 	);
 }
 
-export function MobileNav() {
-	return (
-		<div className="left-0 bottom-0 z-50 flex w-full bg-white md:hidden">
-			{ROUTES.map((routeProps) => (
-				<MobileNavItem key={routeProps.name} {...routeProps}>
-					{routeProps.name}
-				</MobileNavItem>
-			))}
-		</div>
-	);
-}
-
 export default function Nav() {
 	return (
 		<>
 			<div className="fixed top-0 left-0 z-20 hidden h-24 w-full bg-white bg-opacity-75 backdrop-blur-xl md:block"></div>
-			<div className="sticky top-6 z-30 my-12 hidden w-full justify-center md:flex">
+			<div className="z-30 flex w-full justify-center md:sticky md:top-6 md:my-12">
 				<NavPills />
 			</div>
-			<MobileNav />
 		</>
 	);
 }
