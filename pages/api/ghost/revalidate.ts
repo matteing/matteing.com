7999c5cb472c952b707c983939b3406d22a3b304
Sergio Mattei <sergiomattei@outlook.com>
2022-10-06 09:20:28 -0400

    Rewrite revalidator for ISR

--- pages/api/ghost/revalidate.ts
+++ pages/api/ghost/revalidate.ts
@@ -1,48 +1,67 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import get from "lodash/fp/get";
-import { DISCORD_HOOK, REVALIDATE_KEY } from "../../../config";
+import { REVALIDATE_KEY } from "../../../config";
+import { postMessage } from "../../../lib/discord";
+import isError from "lodash/isError";
+import reduce from "lodash/reduce";
 
-async function postNotification(prev: string, current: string) {
-	if (DISCORD_HOOK) {
-		await fetch(DISCORD_HOOK, {
-			method: "POST",
-			headers: {
-				Accept: "application/json",
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({
-				content: `Updated: previous: ${prev}, current: ${current} `,
-			}),
-		});
-	}
+interface RevalidateMap {
+	[type: string]: (slug: string) => string;
+}
+
+const PATH_MAP: RevalidateMap = {
+	post: (slug) => `/posts/${slug}`,
+	page: (slug) => `/${slug}`,
+};
+
+interface SlugItem {
+	type: "post" | "page";
+	slug: string;
+}
+
+function collectSlugs(req: NextApiRequest): SlugItem[] {
+	return reduce(
+		Object.keys(req.body),
+		(result: SlugItem[], value) => {
+			const currentSlug = get(`${value}.current.slug`, req.body);
+			const previousSlug = get(`${value}.previous.slug`, req.body);
+			if (previousSlug === undefined && currentSlug) {
+				return [
+					...result,
+					{ type: value as SlugItem["type"], slug: currentSlug },
+				];
+			} else if (currentSlug === undefined && previousSlug) {
+				return [
+					...result,
+					{ type: value as SlugItem["type"], slug: previousSlug },
+				];
+			} else {
+				return [
+					...result,
+					{ type: value as SlugItem["type"], slug: currentSlug },
+					{ type: value as SlugItem["type"], slug: previousSlug },
+				];
+			}
+		},
+		[]
+	);
 }
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
 	// Check for secret to confirm this is a valid request
-	// eslint-disable-next-line no-console
-	console.log(req.query.secret, REVALIDATE_KEY);
 	if (req.query.secret !== REVALIDATE_KEY) {
 		return res.status(401).json({ message: "Invalid token" });
 	}
 
 	try {
-		// this should be the actual path not a rewritten path
-		// e.g. for "/blog/[slug]" this should be "/blog/post-1"
-		if (req.body?.post) {
-			const currentSlug = get("post.current.slug", req.body);
-			const previousSlug = get("post.previous.slug", req.body);
-			if (currentSlug) await res.revalidate(`/posts/${currentSlug}`);
-			if (previousSlug !== currentSlug)
-				await res.revalidate(`/posts/${previousSlug}`);
-			postNotification(previousSlug, currentSlug);
-		} else if (req.body?.page) {
-			const currentSlug = get("page.current.slug", req.body);
-			const previousSlug = get("page.previous.slug", req.body);
-			if (currentSlug) await res.revalidate(`/${currentSlug}`);
-			if (previousSlug !== currentSlug)
-				await res.revalidate(`/${previousSlug}`);
-			postNotification(previousSlug, currentSlug);
-		}
+		const collected = collectSlugs(req.body);
+		await Promise.all(
+			collected.map(async (item) => {
+				const path = PATH_MAP[item.type](item.slug);
+				await res.revalidate(path);
+				await postMessage(`✅ Revalidated w/ ISR: ${path}`);
+			})
+		);
 		return res.json({ revalidated: true });
 	} catch (err) {
 		// If there was an error, Next.js will continue
@@ -50,20 +69,8 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 		// eslint-disable-next-line no-console
 		console.error(err);
 		let message = "Unknown Error";
-		// eslint-disable-next-line lodash/prefer-lodash-typecheck
-		if (err instanceof Error) message = err.message;
-		if (DISCORD_HOOK) {
-			await fetch(DISCORD_HOOK, {
-				method: "POST",
-				headers: {
-					Accept: "application/json",
-					"Content-Type": "application/json",
-				},
-				body: JSON.stringify({
-					content: `Failed to on-demand revalidate. ${message}`,
-				}),
-			});
-		}
+		if (isError(err)) message = err.message;
+		postMessage(`⚠️ Failed to on-demand revalidate. ${message}`);
 		return res.status(500).send("Error revalidating");
 	}
 };
